63d3daede232e11d7b9fca67cb833540404a267a
daniel54bhp <69922878+daniel54bhp@users.noreply.github.com>
2021-03-17 22:38:01 -0400

    Add files via upload

---
+++ js/ojo.js
@@ -0,0 +1,11 @@
+
+var movimiento= document.getElementsByClassName('pupilas','pupilas12')
+document.onmousemove= function(){
+    var x= event.clientX * 100 / window.innerWidth + "%";
+    var y= event.clientY * 100 / window.innerWidth + "%";
+    for(var i=0;i<2;i++){
+        movimiento[i].style.left=x;
+        movimiento[i].style.top=y;
+        movimiento[i].style.transform = "translate(- "+x+",-"+y+")";
+    }
+}
