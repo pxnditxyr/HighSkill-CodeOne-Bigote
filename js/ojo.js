9ac325ad073938efe05e8371875d605e9050dda3
Yuki xD <pxnditxyr@gmail.com>
2021-03-21 00:19:00 -0400

    Cambio del script del ojo seguidor del cursor

--- js/ojo.js
+++ js/ojo.js
@@ -1,5 +1,5 @@
-
-let movimiento = document.getElementsByClassName('pupila')
+/*
+var movimiento = document.getElementsByClassName('pupila')
 document.onmousemove = function() {
     let x = event.clientX * 100 / window.innerWidth + "%";
     let y = event.clientY * 100 / window.innerWidth + "%";
@@ -9,3 +9,13 @@ document.onmousemove = function() {
         movimiento[i].style.transform = "translate(- " + x + ",-" + y + ")";
     }
 }
+*/
+
+const pupila = document.querySelector(".pupila");
+
+document.addEventListener("mousemove", (event) => {
+	let x = event.clientX * 100 / window.innerWidth - 30;
+	let y = event.clientY * 100 / window.innerHeight - 30;
+	pupila.style.left = x + "%";
+	pupila.style.top = y + "%";
+})
